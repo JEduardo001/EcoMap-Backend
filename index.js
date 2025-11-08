eb86f195062d976434fae47b19b53b5a7c4365f0
EduardoQA <eduardoarce773@gmail.com>
2025-11-07 21:38:17 -0700

    vercel

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const express = require("express");
+const app = express();
+
+const userRoutes = require("./routes/userRoutes");
+const markers = require("./routes/markers");
+const speciesRoutes = require("./routes/speciesRoutes");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/users", userRoutes);
+app.use("/markers", markers);
+app.use("/species", speciesRoutes);
+
+module.exports = app;
+
+
+/* 
+
 // index.js
 const express = require("express");
 const app = express();
@@ -20,3 +39,6 @@ app.use((req, res, next) => {
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
+
+
+*/
